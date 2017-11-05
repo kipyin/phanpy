2854b8a9f692b9d5578aabe616f58e01a965abdd
kip <kipyyin@gmail.com>
2017-11-05 11:13:44 +0800

    add config.py for version control

---
+++ config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  5 11:08:19 2017
+
+@author: Kip
+"""
+
+from core.helpers import which_version
+
+VERSION_GROUP_ID, REGION_ID, VERSION_ID = which_version('platinum')
--- tables.py
+++ tables.py
@@ -7,7 +7,7 @@ Created on Sat Nov  4 17:20:44 2017
 """
 from pandas import read_csv
 
-from core.helpers import which_version
+from config import VERSION_GROUP_ID, REGION_ID
 
 
 path = './data/csv/'
@@ -18,7 +18,11 @@ with open(path + 'experience.csv') as csv_file:
     experience = read_csv(csv_file)
 
 with open(path + 'moves.csv') as csv_file:
+
     moves = read_csv(csv_file)
+
+    __condition = moves["generation_id"] <= REGION_ID
+    moves = moves[__condition]
 
 with open(path + 'move_meta.csv') as csv_file:
     move_meta = read_csv(csv_file)
@@ -36,7 +40,11 @@ with open(path + 'pokemon_abilities.csv') as csv_file:
     pokemon_abilities = read_csv(csv_file)
 
 with open(path + 'pokemon_moves.csv') as csv_file:
+
     pokemon_moves = read_csv(csv_file)
+
+    __condition = pokemon_moves["version_group_id"] == VERSION_GROUP_ID
+    pokemon_moves = pokemon_moves[__condition]
 
 with open(path + 'pokemon_species.csv') as csv_file:
     pokemon_species = read_csv(csv_file)
@@ -44,20 +52,15 @@ with open(path + 'pokemon_species.csv') as csv_file:
 with open(path + 'pokemon_stats.csv') as csv_file:
     pokemon_stats = read_csv(csv_file)
 
-with open(path + 'pokemon_types.csv') as csv_file:
-    pokemon_types = read_csv(csv_file)
+if REGION_ID <= 5:
+    with open(path + 'pokemon_types.csv') as csv_file:
+        pokemon_types = read_csv(csv_file)
+else:
+    with open(path + 'pokemon_types_gen_6.csv') as csv_file:
+        pokemon_types = read_csv(csv_file)
 
 with open(path + 'pokemon.csv') as csv_file:
     pokemon = read_csv(csv_file)
 
 with open(path + 'types.csv') as csv_file:
     types = read_csv(csv_file)
-
-
-VERSION_GROUP_ID, REGION_ID, VERSION_ID = which_version('platinum')
-
-__condition = pokemon_moves["version_group_id"] == VERSION_GROUP_ID
-pokemon_moves = pokemon_moves[__condition]
-
-__condition = moves["generation_id"] <= REGION_ID
-moves = moves[__condition]
