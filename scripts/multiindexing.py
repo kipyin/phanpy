c167c4f9bd6bd7b3d064c80f3acb9cf9aa0f6153
kip <kipyyin@gmail.com>
2017-11-08 02:27:11 +0800

    update multi indexing script

--- scripts/multiindexing.py
+++ scripts/multiindexing.py
@@ -5,25 +5,7 @@ from collections import defaultdict
 import pandas as pd
 import numpy as np
 
-"""
-info = list(zip(*[['info'] * 3, ['order', 'move', 'item']]))
 
-damage = list(zip(*[['damage'] * 2, ['self', 'op']]))
-
-status = list(zip(*[['status'] * 2, ['self','op']]))
-
-stats = list(zip(*[['stats'] * 8, ['attack', 'defense',
-                                   'specialAttack', 'specialDefense',
-                                   'speed', 'critical',
-                                   'accuracy', 'evasion']]))
-
-(info, damage, status, stats) = (pd.MultiIndex.from_tuples(x) for x in
-                                 [info, damage, status, stats])
-"""
-
-#events = pd.DataFrame(index=np.arange(1,51),
-#                      columns=['order', 'move', 'item', 'self', 'opponent'],
-#                      data=np.zeros((50,5)))
 
 statnames = ['attack', 'defense', 'specialAttack', 'specialDefense',
              'speed', 'critical', 'accuracy', 'evasion']
@@ -46,7 +28,7 @@ for top in ['order', 'move', 'item']:
     events[top] = __zeros
 
 for part in ['self', 'opponent']:
-    events.loc[:, (part, 'ailment')] = __zeros
+    events.loc[:, [part, 'ailment'] = __zeros
 
 events.index.names = ['turn']
 
